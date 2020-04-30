fdd735eeae9181481e62c4756e6a2e4684792ac5
Arulraj A <arul.raj47@gmail.com>
2020-04-30 16:26:09 +0530

    Committed the example project.

---
+++ Data/Connections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlazorServerApp
+{
+    public class Connections
+    {
+        public static string ConnectionString { get; set; } = "DefaultEndpointsProtocol=https;AccountName=pdfdemodocuments;AccountKey=ByfXqQPKnH4Mj/1bC+uE0OG6OTzPbDv31CQcgRLZxfJoLssJYyHY3/KMtktpXuVcPxubPowSWAp2ujJ5b7he5w==;EndpointSuffix=core.windows.net";
+    }
+}
